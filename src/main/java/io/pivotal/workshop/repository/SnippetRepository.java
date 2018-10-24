82362f1527528f69245a754669c9da4afaadd4f4
Administrator <Administrator@win2012base>
2018-10-24 06:35:20 -0700

    code-snippet-manager-mongo

---
+++ src/main/java/io/pivotal/workshop/repository/SnippetRepository.java
@@ -0,0 +1,8 @@
+package io.pivotal.workshop.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import io.pivotal.workshop.domain.Snippet;
+
+public interface SnippetRepository extends CrudRepository<Snippet, String> {
+
+}
